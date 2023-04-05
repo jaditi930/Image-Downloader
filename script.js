a8ad18460d7845aeb0692630b9b4ed128226e939
Aditi Jain <100277545+jaditi930@users.noreply.github.com>
2023-04-05 23:30:19 +0530

    Added download feature

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var imageURL;
 function get_image() {
   document.getElementById("image").src="loader.gif";
   let keyword = document.getElementById("search").value;
@@ -17,4 +18,20 @@ fetch(api).then(
         document.getElementById("dwld_btn").style.display="block";
         });
 
+}
+function download_image()
+{
+    fetch(imageURL)
+    .then(resp => resp.blob())
+    .then(blobobject => {
+        const blob = window.URL.createObjectURL(blobobject);
+        const anchor = document.createElement('a');
+        anchor.style.display = 'none';
+        anchor.href = blob;
+        anchor.download = document.getElementById("search").value;
+        document.body.appendChild(anchor);
+        anchor.click();
+        window.URL.revokeObjectURL(blob);
+    })
+    .catch(() => console.log('An error in downloading the file sorry'));
 }
